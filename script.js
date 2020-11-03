e83817fd7ba6905bbd664f55b2dd09f780b585b8
Sergey <fserega2014@yandex.ru>
2020-11-03 20:49:11 +0300

    Это наш первый комит

---
+++ script.js
@@ -0,0 +1,37 @@
+'use strict'
+let persone = {
+  name: 'John',
+  age: 25,
+  isMarried: false
+};
+
+//console.log(persone['name']);
+
+//let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+
+//console.log(arr[0])
+
+//let answer = +prompt('Сколько тебе лет?')
+
+//console.log(typeof(answer));
+//console.log('arr' + ' - object');
+//console.log(4 + +' - object');
+
+let incr = 10,
+    decr = 10;
+
+
+console.log(incr++ + ' ' + decr--)
+console.log(incr + ' ' + decr)
+
+console.log(5%2);
+
+console.log('2' === 2);
+
+let isChecked = true,
+    isClose = false;
+
+console.log(isChecked && !isClose);
+
+let a=5;
+console.log(a);
